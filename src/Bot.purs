35340f65054f182acc50abb14d93ebcd66d71ff2
Pascal Hartig <phartig@rdrei.net>
2016-07-22 19:20:52 +0200

    Make eval step pure for now

--- src/Bot.purs
+++ src/Bot.purs
@@ -1,27 +1,26 @@
 module Bot where
 
 import Prelude
-
 import Bot.Types as Bot
 import Data.String as String
 import Network.HTTP.Affjax as Affjax
 import Text.Parsing.StringParser.Combinators as Parser
 import Text.Parsing.StringParser.String as StringParser
-
+import Bot.Types (MessageResponse(RspNoop))
 import Control.Alt ((<|>))
 import Control.Monad.Aff (launchAff)
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Console (info, CONSOLE, log)
 import Control.Monad.Eff.Exception (EXCEPTION)
 import Control.Monad.Eff.Exception.Unsafe (unsafeThrow)
 import Data.Either (Either(Left, Right))
+import Data.Foreign (Foreign)
 import Data.HTTP.Method (Method(POST))
 import Data.List (List, toUnfoldable)
 import Data.Maybe (Maybe(Just))
 import Network.HTTP.Affjax (AJAX)
 import Text.Parsing.StringParser (Parser, runParser)
 import Text.Parsing.StringParser.String (skipSpaces)
-import Data.Foreign (Foreign())
 
 listToString :: List Char -> String
 listToString = String.fromCharArray <<< toUnfoldable
@@ -49,19 +48,19 @@ handleReceivedMessage
 handleReceivedMessage config ev@(Bot.MessagingEvent e) =
   let txt ({ message: Bot.Message { text: text } }) = text
       res = runParser commandParser (txt e)
-  in case res of
-    Left err -> log $ "Unable to handle message: " <> show err
-    Right cmd -> evalCommand config e.sender cmd
+      msg = case res of
+        -- TODO: Make this an error response message.
+        Left err -> RspNoop
+        Right cmd -> evalCommand e.sender cmd
+  in void <<< launchAff $ callSendAPI config msg
 
 evalCommand
-  :: forall e. Bot.MessengerConfig
-  -> Bot.MessagingParticipant
+  :: Bot.MessagingParticipant
   -> Bot.Command
-  -> Eff (err :: EXCEPTION, ajax :: AJAX | e) Unit
-evalCommand conf senderId = go
+  -> Bot.MessageResponse
+evalCommand senderId = go
   where go (Bot.CmdSubscribe channel) =
-          let rsp = Bot.RspText { text: "Hoo-fucking-ray!", recipient: senderId }
-          in void <<< launchAff $ callSendAPI conf rsp
+          Bot.RspText { text: "Hoo-fucking-ray!", recipient: senderId }
         go (Bot.CmdUnsubscribe channel) = unsafeThrow "unsubscribe not implemented"
 
 callSendAPI :: forall e. Bot.MessengerConfig -> Bot.MessageResponse -> Affjax.Affjax e Foreign
